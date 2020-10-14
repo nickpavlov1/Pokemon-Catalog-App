7f20f7876b06970a90c8ca1cf0b7c6288c83888e
Nikolay Pavlov <nikolaypavlov4@gmail.com>
2020-10-14 03:50:13 +0300

    Adding a func. to select a Pokemon, generate a random opponent and visualise their images, names and HP on the canvas. #2

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const getPokemon = () => { // Fetching the first 20 pokemon from the Pokemon API.
     fetch('https://pokeapi.co/api/v2/pokemon')
     .then(response => response.json())
@@ -9,8 +8,8 @@ const getPokemon = () => { // Fetching the first 20 pokemon from the Pokemon API
     });
 };
 
-const fetchEachPokemon = (pokemon) => { // Using the individual URLs for each pokemon to fetch further information for their template.
-    let pokeUrl = pokemon.url; // <-- Individual URL; example: https://pokeapi.co/api/v2/pokemon/1/
+const fetchEachPokemon = pokemon => { // Using the individual URLs for each pokemon to fetch further information for their template.
+    const pokeUrl = pokemon.url; // <-- Individual URL; example: https://pokeapi.co/api/v2/pokemon/1/
 
     fetch(pokeUrl)
     .then(response => response.json())
@@ -19,22 +18,26 @@ const fetchEachPokemon = (pokemon) => { // Using the individual URLs for each po
     });
 };
 
-const renderPokemonTemplate = (fetchedPokemon) => { 
-    let pokemonTemplateContainer = document.getElementById('pokemon-container'); // Calling for the container wrapping all templates. 
+const renderPokemonTemplate = fetchedPokemon => { 
+    const pokemonTemplateContainer = document.getElementById('pokemon-container'); // Calling for the container wrapping all templates. 
 
-    let pokemonTemplate = document.createElement('div'); // Render individual template container for each pokemon.
+    const pokemonTemplate = document.createElement('div'); // Render individual template container for each pokemon.
     pokemonTemplate.classList.add('pokemon-template');
     pokemonTemplate.id = fetchedPokemon.id;
 
-    let pokemonImg = document.createElement('img'); // Render Pokemon avatar
+    const pokemonImg = document.createElement('img'); // Render Pokemon avatar
     pokemonImg.id = 'poke-avatar';
-    pokemonImg.srcset = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${fetchedPokemon.id}.png`; // URL to default pokemon avatar.
+    pokemonImg.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${fetchedPokemon.id}.png`; // URL to default pokemon avatar.
 
-    let pokemonName = document.createElement('h2'); // Render name
+    const pokemonAttackImg = document.createElement('img'); 
+    pokemonAttackImg.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/${fetchedPokemon.id}.png`;
+    pokemonAttackImg.style.display = 'none';
+
+    const pokemonName = document.createElement('h2'); // Render name
     pokemonName.id = 'pokemon-name';
     pokemonName.innerText = capitalize(fetchedPokemon.name);
 
-    let pokemonAbililty = document.createElement('p');  // Render Pokemon ability p element
+    const pokemonAbililty = document.createElement('p');  // Render Pokemon ability p element
     pokemonAbililty.className = 'poke-stats';
     for (let i = 0; i < fetchedPokemon.abilities.length; i++) { 
 
@@ -45,37 +48,38 @@ const renderPokemonTemplate = (fetchedPokemon) => {
         };
     };
 
-    let pokemonMoves = document.createElement('div');  // Rendering div element to contain the rendered moves.
+    const pokemonMoves = document.createElement('div');  // Rendering div element to contain the rendered moves.
     pokemonMoves.className = 'poke-stats';
 
     setMovesList(fetchedPokemon, pokemonMoves); // Render moves list.
 
-    let pokemonSpeed = document.createElement('p'); // Render Pokemon speed.
+    const pokemonSpeed = document.createElement('p'); // Render Pokemon speed.
     pokemonSpeed.className = 'poke-stats';
     pokemonSpeed.innerText = `Speed: ${fetchedPokemon.stats[5].base_stat}`;
 
-    let pokemonSpDef = document.createElement('p'); // Render Pokemon special defence.
+    const pokemonSpDef = document.createElement('p'); // Render Pokemon special defence.
     pokemonSpDef.className = 'poke-stats';
     pokemonSpDef.innerText = `Special Defence: ${fetchedPokemon.stats[4].base_stat}`;
 
-    let pokemonSpAtt = document.createElement('p'); // Render Pokemon special attack.
+    const pokemonSpAtt = document.createElement('p'); // Render Pokemon special attack.
     pokemonSpAtt.className = 'poke-stats';
     pokemonSpAtt.innerText = `Special Attack: ${fetchedPokemon.stats[3].base_stat}`;
 
-    let pokemonDef = document.createElement('p'); // Render Pokemon defence.
+    const pokemonDef = document.createElement('p'); // Render Pokemon defence.
     pokemonDef.className = 'poke-stats';
     pokemonDef.innerText = `Defence: ${fetchedPokemon.stats[2].base_stat}`;
 
-    let pokemonAttack = document.createElement('p'); // Render Pokemon attack.
+    const pokemonAttack = document.createElement('p'); // Render Pokemon attack.
     pokemonAttack.className = 'poke-stats';
     pokemonAttack.innerText = `Attack: ${fetchedPokemon.stats[1].base_stat}`;
 
-    let pokemonHp = document.createElement('p'); // Render Pokemon health points.
+    const pokemonHp = document.createElement('p'); // Render Pokemon health points.
     pokemonHp.className = 'poke-stats';
     pokemonHp.innerText = `HP: ${fetchedPokemon.stats[0].base_stat}`;
 
     pokemonTemplate.append( // Appeding all the details to the Pokemon template.
         pokemonImg,
+        pokemonAttackImg,
         pokemonName,
         pokemonAbililty,
         pokemonMoves,
@@ -86,15 +90,17 @@ const renderPokemonTemplate = (fetchedPokemon) => {
         pokemonAttack,
         pokemonHp
     );
-    pokemonTemplateContainer.appendChild(pokemonTemplate); // Appending each template container that holds each Pokemon's details to the main div container.
 
+    selectPokemon(pokemonTemplate); // Click event to select a Pokemon to fight a random Pokemon from the catalog.
+
+    pokemonTemplateContainer.appendChild(pokemonTemplate); // Appending each template container that holds each Pokemon's details to the main div container.
 
 };
 
 const setMovesList = (pokemon, div) => { // Function to get the first four moves from the Pokemons moves list.
 
     for (let i = 0; i <= 3; i++) {
-        let pokemonMove = document.createElement('p');
+        const pokemonMove = document.createElement('p');
         pokemonMove.innerText = `Move #${i+1}: ${capitalize(pokemon.moves[i].move.name)}`; // Setting move number and name.
         div.append(pokemonMove);
     };
@@ -105,4 +111,63 @@ const capitalize = word => { // Change the first letter of a given word to upper
     return capitalizedWord;
 }
 
+const selectPokemon = selectedPokemon => {
+
+    // Function containing a click event that visualises a selected Pokemon and random Pokemon in the canvas element and inciating a battle between them.
+
+    selectedPokemon.addEventListener('click', () => { 
+
+        const canvas = document.getElementById('poke-canvas');
+
+        if (isCanvasEmpty(canvas)) { // Checking if the canvas is empty to generate a new battle. Using it not to overlap events.
+            
+            const context = canvas.getContext('2d');
+            context.font = "26px Calibri";
+            
+            const selectedPokemonImg = document.getElementById(selectedPokemon.id).firstElementChild.nextSibling; // Using the selected Pokemon's ID to get it's backwards image.
+            const selectedPokemonName = selectedPokemonImg.nextSibling.innerText; // Selecting the element containing the chosen Pokemon's name.
+            const selectedPokemonFullHP = document.getElementById(selectedPokemon.id).lastElementChild.innerText.split(" ")[1]; // Getting the chosen Pokemon's HP and turning them itno a number.
+            let selectedPokemonCurrentHP = selectedPokemonFullHP; // Creating a variable containing the HP that will be decreased and visualised during the battle.
+
+            context.drawImage(selectedPokemonImg, 30, 220, 150, 150); // Visualising the selected Pokemon's image on the canvas.
+            context.fillText(selectedPokemonName, 260, 265); // // Visualising the selected Pokemon's name.
+            context.fillText(`HP: ${selectedPokemonCurrentHP}/${selectedPokemonFullHP}`, 260, 295); // Visualising the selected Pokemon's current HP and total HP.
+
+            const randomOpponentId = pickRandomOpponent(selectedPokemon.id); // Generating a random ID to chose a opponent.
+
+            const opponentPokemon = document.getElementById(randomOpponentId); // Getting opponent's template.
+            const opponentImg = opponentPokemon.firstElementChild; // Getting opponent's image.
+            const opponentName = opponentImg.nextSibling.nextSibling.innerText; // Getting opponent's name.
+            const opponentFullHP = opponentPokemon.lastElementChild.innerText.split(" ")[1]; // Getting opponent's HP and turning them into a number.
+            let opponentCurrentHP = opponentFullHP; // Creating a variable containing the HP that will be decreased and visualised during the battle.
+
+            context.drawImage(opponentImg, 270, 20, 150, 150); // Visualising opponent's image on the canvas.
+            context.fillText(opponentName, 80, 50); // Visualising opponet's name.
+            context.fillText(`HP: ${opponentCurrentHP}/${opponentFullHP}`, 80, 80); // Visualising the oppponent's current HP and total HP.
+        };
+    });
+};
+
+
+const pickRandomOpponent = yourPokemonId => { // Picking a random number to generate for the opponent's ID without repeating the selected Pokemon's ID.
+    let randomOpponentId = Math.floor(Math.random() * 20) + 1;
+
+    if (yourPokemonId == randomOpponentId) {
+        if (randomOpponentId === 20) {
+            randomOpponentId -= 1;
+        } else {
+            randomOpponentId += 1;
+        };
+    };
+    return randomOpponentId;
+};
+
+const isCanvasEmpty = canvas => { // Checking if the canvas is empty. 
+    const blankCanvas = document.getElementById('poke-canvas');
+
+    blankCanvas.width = canvas.width;
+    blankCanvas.height = canvas.height;
+
+    return canvas.toDataURL() === blankCanvas.toDataURL(); // Returns true if empty.
+};
 getPokemon(); // Render all Pokemon templates. 
